96d86b30223c9ecfcb94955910cb4e5bdf40a47d
omgzsa <zselloandras@gmail.com>
2024-10-25 17:33:06 +0200

    @style: update styles by prop

--- components/question/block.vue
+++ components/question/block.vue
@@ -3,19 +3,24 @@ defineProps({
   title: String,
   placement: String,
   subText: String,
+  bordered: {
+    type: Boolean,
+    default: false,
+  },
 });
 </script>
 
 <template>
   <div
-    class="relative flex flex-col max-w-2xl py-6 mx-auto space-y-4 border-b border-b-neutral-200"
+    class="relative flex flex-col max-w-2xl p-2 mx-auto space-y-4 border-b xs:p-3 sm:p-6 lg:p-8 border-b-neutral-200"
+    :class="{ 'border rounded-md bg-neutral-100/90 z-0 pb-6 border-neutral-500 border-b-neutral-500 dark:bg-neutral-800' : bordered }"
   >
     <slot name="hint" />
     <h2 class="pb-3">
       <span>{{ placement }}</span> {{ title }}
     </h2>
     <p v-if="subText" class="pb-3">{{ subText }}</p>
-    <div class="space-y-2">
+    <div class="pb-4 space-y-3">
       <slot />
     </div>
   </div>
