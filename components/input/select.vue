1b9fc202fd48c8836d3cb159de3a3b30c868beee
omgzsa <zselloandras@gmail.com>
2024-10-25 17:32:43 +0200

    select, text component

---
+++ components/input/select.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup>
+import { useField } from 'vee-validate';
+
+const props = defineProps<{
+  name: string,
+  modelValue: string | number,
+  options: string[],
+  label?: string,
+  placeholder?: string,
+  disabled?: boolean,
+}>();
+
+
+const emit = defineEmits(['update:modelValue']);
+
+const { value } = useField(() => props.name, undefined, {
+  syncVModel: true,
+  initialValue: props.modelValue,
+});
+
+watch(() => props.modelValue, (newValue) => {
+  value.value = newValue;
+});
+
+
+watch(value, (newValue) => {
+  emit('update:modelValue', newValue); 
+});
+</script>
+
+<template>
+  <div class="flex items-center gap-3">
+    <label :for="props.name" class="text-xs md:text-sm whitespace-nowrap">
+      {{ props.label }}
+    </label>
+    <USelectMenu 
+      v-model="value" 
+      :options="options"
+      v-slot="{ open }"
+      :ui="{
+        background: 'bg-white dark:bg-neutral-900',
+      }"
+      class="w-full pr-1 sm:w-28"
+    >
+      <UButton color="white" variant="outline" class="justify-between flex-1 py-[2.25px] text-xs font-bold transition-all border-0 border-none rounded-md shadow-sm ring-1 focus:ring-paktum-500 hover:border-neutral-950 focus:border-paktum-500 md:text-sm dark:bg-neutral-800 dark:text-neutral-100 text-neutral-900 ring-inset ring-gray-300 placeholder:text-gray-400 hover:bg-none focus:ring-2 focus:ring-inset">
+        {{ value }}
+
+        <UIcon 
+          name="i-heroicons-chevron-right-20-solid" 
+          class="w-5 h-5 transition-transform" :class="[open && 'transform rotate-90']" 
+        />
+      </UButton>
+    </USelectMenu>
+  </div>
+</template>
+
+<style scoped>
+/* */
+</style>
--- components/input/text.vue
+++ components/input/text.vue
@@ -1,35 +1,51 @@
-<script setup>
-const props = defineProps({
-  name: String,
-  label: String,
-});
-const { value, errorMessage, meta } = useField(() => props.name, undefined, {
-  syncVModel: true,
-});
+<script lang="ts" setup>
+const props = defineProps<{
+  name: string;
+  type?: string;
+  label?: string;
+  labelEnd?: string;
+  placeholder?: string;
+  isDisabled?: boolean;
+}>();
+
+const model = defineModel();
+
+const { value, errorMessage } = useField(
+  `${props.name}`,
+  undefined,
+  {
+    syncVModel: true,
+    initialValue: model.value,
+  }
+);
+
+const inputId = `${props.name}-input`;
 </script>
 
 <template>
-  <div class="app-input-wrapper">
-    <label class="app-input-label" :for="name">
-      {{ label }}
-    </label>
-    <input v-model="value"/>
-    <Transition name="pop-bottom">
-      <span v-if="errorMessage && meta.touched" role="alert">{{
-        errorMessage
-      }}</span>
-    </Transition>
+  <div class="space-y-1">
+    <div class="flex items-center gap-2">
+      <label
+        v-if="props.label"
+        :for="inputId"
+        class="text-xs md:text-sm whitespace-nowrap"
+      >
+        {{ props.label }}
+      </label>
+      <input
+        v-model="value"
+        :id="inputId"
+        :disabled="props.isDisabled"
+        :type="props.type || 'text'"
+        :placeholder="props.placeholder"
+        class="mx-1 inline flex-wrap h-6 transition-all border-none ring-1 focus:ring-paktum-500 md:px-2 md:pb-1.5 hover:border-neutral-950 focus:border-paktum-500 font-bold text-xs md:text-sm dark:bg-neutral-800 dark:text-neutral-100"
+        :class="{ 'cursor-not-allowed opacity-50': props.isDisabled }"
+      />
+      <span v-if="props.labelEnd" class="text-xs md:text-sm">{{ props.labelEnd }}</span>
+    </div>
+    <!-- Error message rendering -->
+    <span v-if="errorMessage" class="text-xs text-red-500">
+      {{ errorMessage }}
+    </span>
   </div>
 </template>
-
-<style>
-.pop-bottom-enter-active,
-.pop-bottom-leave-active {
-  transition: all 0.2s ease;
-}
-.pop-bottom-enter-from,
-.pop-bottom-leave-to {
-  opacity: 0;
-  transform: translateY(10px);
-}
-</style>
