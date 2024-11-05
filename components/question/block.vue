7112054f47069043c2d6d811eb3deb1fa09dfe4d
omgzsa <zselloandras@gmail.com>
2024-11-05 12:08:40 +0100

    style: update

--- components/question/block.vue
+++ components/question/block.vue
@@ -3,6 +3,7 @@ defineProps({
   title: String,
   placement: String,
   subText: String,
+  price: String,
   bordered: {
     type: Boolean,
     default: false,
@@ -12,15 +13,20 @@ defineProps({
 
 <template>
   <div
-    class="relative flex flex-col max-w-2xl p-2 mx-auto space-y-4 border-b xs:p-3 sm:p-6 lg:p-8 border-b-neutral-200"
-    :class="{ 'border rounded-md bg-neutral-100/90 z-0 pb-6 border-neutral-500 border-b-neutral-500 dark:bg-neutral-800' : bordered }"
+    class="relative flex flex-col max-w-2xl px-2 py-4 mx-auto space-y-5 border-b xs:py-5 xs:px-4 md:px-5 lg:px-7 md:py-7 lg:py-9 border-b-neutral-200"
+    :class="{ 'border rounded-md bg-neutral-100/90 z-0 border-neutral-500 border-b-neutral-500 dark:bg-neutral-800' : bordered }"
   >
     <slot name="hint" />
-    <h2 class="pb-3">
-      <span>{{ placement }}</span> {{ title }}
+    <h2 class="flex items-center justify-between gap-2">
+      <div>
+        <span>{{ placement }}</span> {{ title }}
+      </div>
+      <div v-if="price" class="text-xs italic font-normal text-neutral-700 dark:text-neutral-400">
+        {{ price }}
+      </div>
     </h2>
-    <p v-if="subText" class="pb-3">{{ subText }}</p>
-    <div class="pb-4 space-y-3">
+    <p v-if="subText" class="">{{ subText }}</p>
+    <div class="space-y-3">
       <slot />
     </div>
   </div>
