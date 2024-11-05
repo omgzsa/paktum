b7bdfa7d881252cd57b6adcea3c02919ef74acb6
omgzsa <zselloandras@gmail.com>
2024-11-05 12:07:58 +0100

    nav deskt/mob

--- components/nav/desktop.vue
+++ components/nav/desktop.vue
@@ -1,15 +1,21 @@
+<script lang="ts" setup>
+// const router = useRouter();
+
+// function resetRoute() {
+//   router.replace({ path: '/berleti', query: {} }).then(() => {
+//     // Ensures any reactive state resets after navigation
+//     router.go(0); // Forces a reload without leaving the page
+//   });
+// }
+</script>
+
 <template>
-  <nav>
-    <UContainer as="div" class="flex items-center">
+  <nav class="flex items-center">
       <ul class="space-x-4">
         <NuxtLink to="/">home</NuxtLink>
         <NuxtLink to="/berleti">berleti</NuxtLink>
+        <NuxtLink to="/test">test</NuxtLink>
       </ul>
       <TheColorSwitcher />
-    </UContainer>
   </nav>
 </template>
-
-<script lang="ts" setup></script>
-
-<style></style>
