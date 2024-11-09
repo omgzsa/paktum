8c0e419e5c1dc4f342541fb9eeffd6e51e9985de
omgzsa <zselloandras@gmail.com>
2024-11-09 08:11:14 +0100

    new font w added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
 
   googleFonts: {
     families: {
-      Poppins: [100, 200, 300, 400, 500, 700],
+      Poppins: [100, 200, 300, 400, 500, 600, 700],
     },
     // download: true,
   },
