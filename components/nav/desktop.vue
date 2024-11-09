c61edf38f17b76463a74ba1229f6dfef413f03d5
omgzsa <zselloandras@gmail.com>
2024-11-09 08:17:26 +0100

    goin to V2

--- components/nav/desktop.vue
+++ components/nav/desktop.vue
@@ -14,6 +14,7 @@
       <ul class="space-x-4">
         <NuxtLink to="/">home</NuxtLink>
         <NuxtLink to="/berleti">berleti</NuxtLink>
+        <NuxtLink to="/berletiV2/lepes/1">berleti V2</NuxtLink>
         <NuxtLink to="/test">test</NuxtLink>
       </ul>
       <TheColorSwitcher />
