9933cd13fbfcedf0838721407329bc905cec7206
omgzsa <zselloandras@gmail.com>
2024-12-09 22:58:41 +0100

    eslint cfg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,27 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt()
+export default withNuxt(
+  // {
+  //   rules: {
+  //     'vue/max-attributes-per-line': [
+  //       'error',
+  //       {
+  //         'singleline': 1,
+  //         'multiline': {
+  //           'max': 1,
+  //           'allowFirstLine': false
+  //         }
+  //       }
+  //     ],
+  //     'vue/singleline-html-element-content-newline': 'off',
+  //     'vue/multiline-html-element-content-newline': 'off',
+  //     'vue/html-closing-bracket-newline': [
+  //       'error',
+  //       {
+  //         'singleline': 'never',
+  //         'multiline': 'always'
+  //       }
+  //     ]
+  //   }
+  // }
+)
