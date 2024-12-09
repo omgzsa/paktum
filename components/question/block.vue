520144cd5eddc85f65076b101c595a0ad4f9522b
omgzsa <zselloandras@gmail.com>
2024-12-09 11:26:11 +0100

    style update on blocks

--- components/question/block.vue
+++ components/question/block.vue
@@ -14,7 +14,7 @@ defineProps({
 <template>
   <div
     class="flex flex-col max-w-2xl px-2 py-4 mx-auto space-y-5 border-b xs:py-5 xs:px-4 md:px-5 lg:px-7 md:py-7 lg:py-9 border-b-neutral-200 dark:border-b-neutral-600"
-    :class="{ 'border rounded-md bg-neutral-100/90 z-0 border-neutral-500 border-b-neutral-500 dark:bg-neutral-900' : bordered }"
+    :class="{ 'border rounded-md bg-neutral-100/90 z-0 border-neutral-500 border-b-neutral-500 dark:bg-neutral-900 dark:border-neutral-700' : bordered }"
   >
     <slot name="hint" />
     <h2 v-if="title" class="flex items-center justify-between gap-2">
