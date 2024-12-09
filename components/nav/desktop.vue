87ac32481a70c90d507bf6fa8507a0b1baa515c7
omgzsa <zselloandras@gmail.com>
2024-12-09 23:00:44 +0100

    disable non existing page

--- components/nav/desktop.vue
+++ components/nav/desktop.vue
@@ -13,7 +13,7 @@
   <nav class="flex items-center py-4">
     <ul class="space-x-4">
       <NuxtLink to="/">home</NuxtLink>
-      <NuxtLink to="/berleti">berleti</NuxtLink>
+      <!-- <NuxtLink to="/berleti">berleti</NuxtLink> -->
       <NuxtLink to="/berletiV2/lepes/1">berleti V2</NuxtLink>
       <NuxtLink to="/test">test</NuxtLink>
     </ul>
