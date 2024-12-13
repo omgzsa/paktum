3554c36a7eba6d1e8029d751d2da51b13c95651d
omgzsa <zselloandras@gmail.com>
2024-12-13 18:48:38 +0100

    auth related pages added

---
+++ pages/user/login.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+//
+</script>
+
+<template>
+  <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
+    <AuthLogin />
+  </div>
+</template>
