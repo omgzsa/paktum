434de0c01e64c35a43da384012f46a2025546ef8
omgzsa <zselloandras@gmail.com>
2024-12-16 15:35:31 +0100

    added heading to login page (removed from component)

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-  <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
-    <AuthLogin />
-  </div>
+    <div class="flex flex-col items-center justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
+        <h1>Bejelentkezés</h1>
+        <AuthLogin />
+    </div>
 </template>
