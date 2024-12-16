6c979910053cc71a6b248e9f02e8de232a32fd2a
omgzsa <zselloandras@gmail.com>
2024-12-16 15:33:04 +0100

    feat: auth modal w/ controls composable

---
+++ components/auth/modal.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const authModal = useAuthModal()
+</script>
+
+<template>
+  <UModal v-model="authModal.isOpen">
+    <UCard :ui="{ ring: 'ring-0', shadow: 'shadow-none' }">
+      <template #header>
+        <div class="flex items-center justify-between">
+          <h3 class="text-xl font-semibold">Bejelentkezés</h3>
+          <UButton
+            color="gray"
+            variant="ghost"
+            icon="i-heroicons-x-mark"
+            @click="authModal.close"
+          />
+        </div>
+      </template>
+
+      <slot />
+    </UCard>
+  </UModal>
+</template>
---
+++ composables/useAuthModal.ts
@@ -0,0 +1,17 @@
+export const useAuthModal = defineStore('auth-modal', () => {
+  const isOpen = ref(false)
+
+  function open() {
+    isOpen.value = true
+  }
+
+  function close() {
+    isOpen.value = false
+  }
+
+  return {
+    isOpen,
+    open,
+    close
+  }
+})
