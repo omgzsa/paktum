cc7647af6c7926db396eabc1681ee2284412f137
omgzsa <zselloandras@gmail.com>
2024-12-18 17:02:55 +0100

    auth template boilerplates

--- pages/user/register.vue
+++ pages/user/register.vue
@@ -1,13 +1,7 @@
 <template>
-  <div>
-    
-  </div>
+    <div># register</div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
---
+++ pages/user/verification.vue
@@ -0,0 +1,3 @@
+<template>
+    <div># verification</div>
+</template>
