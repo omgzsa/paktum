c79894404aeda878d0f57b1d4d32552690220bf8
omgzsa <zselloandras@gmail.com>
2025-03-25 16:33:04 +0100

    import.meta.env instead of process.env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,47 +1,45 @@
-import { defineNuxtConfig } from "nuxt/config";
-
 export default defineNuxtConfig({
-  app: {
-    pageTransition: { name: "page", mode: "out-in" },
-    layoutTransition: { name: "layout", mode: "out-in" },
-  },
-
-  runtimeConfig: {
-    paktumApiUrl: process.env.NUXT_PAKTUM_API_URL,
-    public: {
-      //
+    app: {
+        pageTransition: { name: 'page', mode: 'out-in' },
+        layoutTransition: { name: 'layout', mode: 'out-in' },
     },
-  },
 
-  colorMode: {
-    preference: "light",
-    fallback: "system",
-  },
-
-  devtools: {
-    enabled: true,
-  },
-
-  modules: [
-    "@nuxt/ui",
-    "@nuxtjs/google-fonts",
-    "@nuxt/image",
-    "@vee-validate/nuxt",
-    "@pinia/nuxt",
-    "@formkit/auto-animate",
-  ],
-
-  googleFonts: {
-    families: {
-      Poppins: [100, 200, 300, 400, 500, 600, 700],
+    runtimeConfig: {
+        paktumApiUrl: import.meta.env.NUXT_PAKTUM_API_URL,
+        public: {
+            //
+        },
     },
-    // download: true,
-  },
 
-  tailwindcss: {
-    cssPath: "~/assets/css/tailwind.css",
-    configPath: "~/tailwind.config.js",
-  },
+    colorMode: {
+        preference: 'light',
+        fallback: 'system',
+    },
 
-  compatibilityDate: "2024-07-19",
-});
+    devtools: {
+        enabled: true,
+    },
+
+    modules: [
+        '@nuxt/ui',
+        '@nuxtjs/google-fonts',
+        '@nuxt/image',
+        '@vee-validate/nuxt',
+        '@pinia/nuxt',
+        '@formkit/auto-animate',
+    ],
+
+    googleFonts: {
+        families: {
+            Poppins: [100, 200, 300, 400, 500, 600, 700],
+        },
+        // download: true,
+    },
+
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: '~/tailwind.config.js',
+    },
+
+    compatibilityDate: '2024-07-19',
+})
